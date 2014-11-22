784266099a0595914ae9bfff6198d10993af6eba
Pedro Kostelec <pedrokost@gmail.com>
2014-11-22 23:50:29 +0100

    Relaxed the map box limits

--- app/views/leaflet-map.js
+++ app/views/leaflet-map.js
@@ -1,8 +1,8 @@
 import MarkerClusterLayer from '../layers/marker-cluster-collection';
 import GoogleMapLayer from '../layers/google-map-layer';
 
-var southWest = L.latLng(45.23, 13.52),   // spodaj levo
-    northEast = L.latLng(47.02, 16.65);   // zgoraj desno
+var southWest = L.latLng(45.0, 13.0),   // spodaj levo
+    northEast = L.latLng(47.2, 17);   // zgoraj desno
 
 export default EmberLeaflet.MapView.extend({
   center: L.latLng(46.2214969, 14.6),     // TODO
