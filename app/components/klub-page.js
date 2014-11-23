d983672ca9388dd685ba4f81c665f265f9548268
Pedro Kostelec <pedrokost@gmail.com>
2014-11-23 17:12:57 +0100

    BUGFIX: Can now select text on the page

--- app/components/klub-page.js
+++ app/components/klub-page.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['klub_page'],
+  classNames: ['klub_page js-close'],
 
-  click: function(){
-    this.sendAction();
+  click: function(evt){
+    // The purpose of whitelisting the close action is to enable selecting
+    // the text on the page to copy it.
+    if (evt.target.classList.contains('js-close')) {
+      this.sendAction();
+    }
   }
 });
