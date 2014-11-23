a3066f3b4d98d876dd58eb58a3447cc2b95f7856
Pedro Kostelec <pedrokost@gmail.com>
2014-11-23 16:24:55 +0100

    Fix rebase conflict issues

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,40 +1,46 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+
   itemController: 'klub',
   zoom: 8,
   center: L.latLng(46.122636,14.81546), // Slivna, Slovenia,
+  hoveredKlub: null,
+  hoveredMarker: null,
+  markers: null,
+
+  layers: function(){
+    return this.map(function(klub){
+      return {
+        location: klub.get('latlng'),
+        id: klub.get('id'),
+        // icon: new L.Icon.Default()
+      };
+    }.bind(this));
+  }.property('@each.latlng'),
+
   isShowPage: function() {
-    console.log(this.get('currentPath'), this.get('currentPath') === 'klub')
     return this.get('currentPath') === 'klub';
-  }.property('currentPath')
+  }.property('currentPath'),
+
+  actions: {
+
+    highlightKlub: function(klubId) {
+      var item = this.findBy('id', klubId);
+      var marker = this.get('layers').findBy('id', klubId);
+
+      // TODO
+      // layer.setIcon(new HighlightedIcon())
+
+      this.set('hoveredKlub', item);
+      // this.set('hoveredMarker', marker)
+    },
+
+    unHighlightKlub: function() {
+      this.set('hoveredKlub', null);
+      // this.get('hoveredMarker').setIcon(new L.Icon.Default());  // TODO
+      // this.set('hoveredMarker', null);
+    }
+  }
 });
 
-
-
-// =======
-// export default Ember.ArrayController.extend({
-//   itemController: 'klub',
-//   hoveredKlub: null,
-//   layers: function(){
-//     var res = this.map(function(klub){
-//       return {
-//         location: klub.get('latlng'),
-//         id: klub.get('id')
-//       };
-//     });
-//     return res;
-//   }.property('@each.latlng'),
-//   actions: {
-//     highlightKlub: function(klubId) {
-//       var item = this.findBy('id', klubId);
-//       this.set('hoveredKlub', item);
-//       // It should them autotrigger the display of the class on the list
-//       // TODO: set all the other to false
-//       // Later: it should also update the marker (highlight)
-//     },
-//     unHighlightKlub: function() {
-//       this.set('hoveredKlub', null);
-//     }
-//   }
-// >>>>>>> Hovering over marker highlights in list
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,40 +1,6 @@
 import Ember from 'ember';
 import HighlightedIcon from '../layers/highlighted-icon';
 
-<<<<<<< HEAD
+
 export default Ember.Controller.extend({
-
-=======
-export default Ember.ArrayController.extend({
-  itemController: 'klub',
-  hoveredKlub: null,
-  hoveredMarker: null,
-  markers: null,
-  layers: function(){
-    return this.map(function(klub){
-      return {
-        location: klub.get('latlng'),
-        id: klub.get('id'),
-        // icon: new L.Icon.Default()
-      };
-    }.bind(this));
-  }.property('@each.latlng'),
-  actions: {
-    highlightKlub: function(klubId) {
-      var item = this.findBy('id', klubId);
-      var marker = this.get('layers').findBy('id', klubId);
-
-      // TODO
-      // layer.setIcon(new HighlightedIcon())
-
-      this.set('hoveredKlub', item);
-      // this.set('hoveredMarker', marker)
-    },
-    unHighlightKlub: function() {
-      this.set('hoveredKlub', null);
-      // this.get('hoveredMarker').setIcon(new L.Icon.Default());  // TODO
-      // this.set('hoveredMarker', null);
-    }
-  }
->>>>>>> Reverted..trying to bind icons unsuccessfully
 });
