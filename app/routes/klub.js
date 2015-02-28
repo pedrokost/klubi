30f263c3fa6f11e5c7e6520e8dc4e9ea0bdb1a9a
Pedro Kostelec <pedrokost@gmail.com>
2015-02-28 14:37:08 +0100

    [IMPROVEMENT] Can navigate to any klub without specifying caterogy in url

--- app/routes/klub.js
+++ app/routes/klub.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('klub', params.klub_id);
+  },
+  setupController: function(controller, model) {
+    // When navigating directly to a klub's page that is
+    // not included in the default category, the model
+    // is unloaded instantly. This fixes this.
+    this._super(controller, model);
+
+    var currentCategory = this.controllerFor('klubs').get('category');
+
+    if (model.get('categories').indexOf(currentCategory) === -1) {
+      this.controllerFor('klubs').set('category', model.get('categories')[0]);
+    }
+
+  }
 });
