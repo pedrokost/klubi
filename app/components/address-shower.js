0dcc8e1e39e06975c0dc34158373543a217f4eff
Pedro Kostelec <pedrokost@gmail.com>
2016-12-17 15:57:19 +0100

    [IMPROVEMENT] Better visual feedback when entering address

--- app/components/address-shower.js
+++ app/components/address-shower.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   address: null,
+  formattedAddress: null,
   geocoder: null,
   marker: null,
   map: null,
   ready: false,
   geocodingInvalid: true,
+  geocodingFailed: false,
   classNames: ['address-shower'],
   classNameBindings: ['geocodingInvalid'],
 
@@ -29,7 +31,6 @@ export default Ember.Component.extend({
     this.set('map', map);
     this.set('marker', marker);
     this.set('ready', true);
-
   }),
 
   showAddressOnMap(position) {
@@ -47,33 +48,49 @@ export default Ember.Component.extend({
 
     var geocoder = this.get('geocoder');
     var address = this.get('address');
+
     var that = this;
 
-    geocoder.geocode( {
-      address,
-      region: 'sl'
-    }, function(results, status) {
-      that.set('geocodingInvalid', status !== google.maps.GeocoderStatus.OK);
+    if (address) {
+      geocoder.geocode( {
+        address,
+        region: 'sl'
+      }, function(results, status) {
+        that.set('geocodingInvalid', status !== google.maps.GeocoderStatus.OK);
+        that.set('geocodingFailed', status !== google.maps.GeocoderStatus.OK);
 
-      if (status == google.maps.GeocoderStatus.OK) {
-        // console.log(results[0].geometry.location);
-        let latitude = results[0].geometry.location.lat();
-        let longitude = results[0].geometry.location.lng();
-        that.set('latitude', latitude);
-        that.set('longitude', longitude);
-        that.get('marker').setOpacity(1);
-        that.sendAction('action', latitude, longitude);
-        that.showAddressOnMap(results[0].geometry.location);
-      } else {
-        console.error('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+        if (status === google.maps.GeocoderStatus.OK) {
+          let latitude = results[0].geometry.location.lat();
+          let longitude = results[0].geometry.location.lng();
+          let formattedAddress = results[0].formatted_address;
+          let town = results[0].address_components.find((component) =>{
+              return component.types.includes('postal_town');
+            }
+          )
+          town = town ? town.long_name : null;
+
+          that.set('latitude', latitude);
+          that.set('longitude', longitude);
+          that.get('marker').setOpacity(1);
+          that.set('formattedAddress', formattedAddress);
+
+          that.sendAction('action', latitude, longitude);
+          // that.sendAction('town', town);
+          // that.sendAction('formattedAddress', formattedAddress)
+
+          that.showAddressOnMap(results[0].geometry.location);
+        }
+      });
+    }
   },
 
   listenForTypingPause: Ember.observer('address', 'ready', function() {
     this.set('latitude', null);
     this.set('longitude', null);
+    this.set('formattedAddress', '...');
+    this.set('town', null);
     this.set('geocodingInvalid', true);
+    this.set('geocodingFailed', false);
     this.get('marker').setOpacity(0.2);
     Ember.run.debounce(this, this.updateMap, 250);
   })
