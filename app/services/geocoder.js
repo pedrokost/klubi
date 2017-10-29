16a131e5e24c1d4e27884653f2d0b450a6ce5fe7
Pedro Kostelec <pedrokost@gmail.com>
2017-10-29 16:52:23 +0100

    [DEV] Change api key for google geocoding service

--- app/services/geocoder.js
+++ app/services/geocoder.js
@@ -1,25 +1,24 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Service.extend({
-
-  baseUrl: 'https://maps.googleapis.com/maps/api/geocode/json?',
-  apiKey: 'AIzaSyCHW25YKlP2Z0ApfyLEEUEeKanMtJTj92Y',
-  language: 'sl',
-  region: 'si',
+  baseUrl: "https://maps.googleapis.com/maps/api/geocode/json?",
+  apiKey: "AIzaSyBpN-jOLvDa0z6-nOaypIrgGLA465bmTSE",
+  language: "sl",
+  region: "si",
   address: null,
 
-  url: Ember.computed('address', function() {
-    const baseUrl = this.get('baseUrl');
-    const apiKey = this.get('apiKey');
-    const language = this.get('language');
-    const region = this.get('region');
-    const address = encodeURIComponent(this.get('address'));
+  url: Ember.computed("address", function() {
+    const baseUrl = this.get("baseUrl");
+    const apiKey = this.get("apiKey");
+    const language = this.get("language");
+    const region = this.get("region");
+    const address = encodeURIComponent(this.get("address"));
 
     return `${baseUrl}language=${language}&api=${apiKey}&region=${region}&address=${address}`;
   }),
 
   geocode(address) {
-    this.set('address', address);
-    return Ember.$.getJSON(this.get('url'));
+    this.set("address", address);
+    return Ember.$.getJSON(this.get("url"));
   }
 });
